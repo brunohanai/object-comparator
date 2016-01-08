e2b786d74fe76e5e7eef08ed09570ba871748330
Bruno Hanai <bruno.hanai@gmail.com>
2016-01-08 18:31:17 -0200

    object adicionado no Diff

--- src/brunohanai/ObjectComparator/Differ/Diff.php
+++ src/brunohanai/ObjectComparator/Differ/Diff.php
@@ -5,17 +5,21 @@ namespace brunohanai\ObjectComparator\Differ;
 class Diff
 {
     const KEY = 'diff';
+    const OBJECT_KEY = 'object';
     const PROPERTY_KEY = 'property';
     const VALUES_KEY = 'values';
     const VALUE_1_KEY = 'value1';
     const VALUE_2_KEY = 'value2';
+    const SEPARATOR = '#';
 
+    private $object;
     private $property;
     private $value1;
     private $value2;
 
-    public function __construct($property, $value_1, $value_2)
+    public function __construct($object, $property, $value_1, $value_2)
     {
+        $this->object = $object;
         $this->property = $property;
         $this->value1 = $value_1;
         $this->value2 = $value_2;
@@ -25,12 +29,13 @@ class Diff
     {
         if ($slim_version === true) {
             return array(
-                $this->property => array($this->value1, $this->value2)
+                $this->object.self::SEPARATOR.$this->property => array($this->value1, $this->value2)
             );
         }
 
         return $array = array(
             self::KEY => array(
+                self::OBJECT_KEY => $this->object,
                 self::PROPERTY_KEY => $this->property,
                 self::VALUES_KEY => array(
                     self::VALUE_1_KEY => $this->value1,
--- tests/brunohanai/ObjectComparator/Differ/DiffTest.php
+++ tests/brunohanai/ObjectComparator/Differ/DiffTest.php
@@ -10,7 +10,7 @@ class DiffTest extends \PHPUnit_Framework_TestCase
         $value1 = 'Hosanna';
         $value2 = 'Hosanna Tecnologia';
 
-        $diff = new Diff($property, $value1, $value2);
+        $diff = new Diff('EmpresaConf', $property, $value1, $value2);
 
         $this->assertEquals($property, $diff->getProperty());
         $this->assertEquals($value1, $diff->getValue1());
@@ -19,14 +19,16 @@ class DiffTest extends \PHPUnit_Framework_TestCase
 
     public function testGetArrayCopy_notUsingSlimVersion_shouldReturnCompleteArray()
     {
+        $object = 'EmpresaConf';
         $property = 'descricao';
         $value1 = 'Hosanna';
         $value2 = 'Hosanna Tecnologia';
 
-        $diff = new Diff($property, $value1, $value2);
+        $diff = new Diff($object, $property, $value1, $value2);
 
         $expectedArray = array(
             Diff::KEY => array(
+                Diff::OBJECT_KEY => $object,
                 Diff::PROPERTY_KEY => $property,
                 Diff::VALUES_KEY => array(
                     Diff::VALUE_1_KEY => $value1,
@@ -39,27 +41,29 @@ class DiffTest extends \PHPUnit_Framework_TestCase
 
     public function testGetArrayCopy_usingSlimVersion_shouldReturnSimpleArray()
     {
+        $object = 'EmpresaConf';
         $property = 'descricao';
         $value1 = 'Hosanna';
         $value2 = 'Hosanna Tecnologia';
 
-        $diff = new Diff($property, $value1, $value2);
+        $diff = new Diff($object, $property, $value1, $value2);
 
-        $expectedArray = array($property => array($value1, $value2));
+        $expectedArray = array($object.Diff::SEPARATOR.$property => array($value1, $value2));
         $this->assertEquals($expectedArray, $diff->getArrayCopy(true));
     }
 
     public function testPrintAsJson_notUsingSlimVersion_shouldReturnCompleteJson()
     {
+        $object = 'EmpresaConf';
         $property = 'descricao';
         $value1 = 'Hosanna';
         $value2 = 'Hosanna Tecnologia';
 
-        $diff = new Diff($property, $value1, $value2);
+        $diff = new Diff($object, $property, $value1, $value2);
 
         $expectedJson = sprintf(
-            '{"%s":{"%s":"%s","%s":{"%s":"%s","%s":"%s"}}}',
-            Diff::KEY, Diff::PROPERTY_KEY, $property,
+            '{"%s":{"%s":"%s","%s":"%s","%s":{"%s":"%s","%s":"%s"}}}',
+            Diff::KEY, Diff::OBJECT_KEY, $object, Diff::PROPERTY_KEY, $property,
             Diff::VALUES_KEY, Diff::VALUE_1_KEY, $value1, Diff::VALUE_2_KEY, $value2
         );
 
@@ -68,13 +72,14 @@ class DiffTest extends \PHPUnit_Framework_TestCase
 
     public function testPrintAsJson_usingSlimVersion_shouldReturnSimpleJson()
     {
+        $object = 'EmpresaConf';
         $property = 'descricao';
         $value1 = 'Hosanna';
         $value2 = 'Hosanna Tecnologia';
 
-        $diff = new Diff($property, $value1, $value2);
+        $diff = new Diff($object, $property, $value1, $value2);
 
-        $expectedJson = sprintf('{"%s":["%s","%s"]}', $property, $value1, $value2);
+        $expectedJson = sprintf('{"%s%s%s":["%s","%s"]}', $object, Diff::SEPARATOR, $property, $value1, $value2);
 
         $this->assertEquals($expectedJson, $diff->printAsJson(true));
     }
